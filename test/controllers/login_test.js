bb065d2849dd23e6fc3cd4329dbdc64cfbdf14a5
SandraOkoli <sandraokoli@hotmail.co.uk>
2017-11-13 12:05:03 +0000

    Login tests complete

--- test/controllers/login_test.js
+++ test/controllers/login_test.js
@@ -3,10 +3,26 @@ require('../spec_helper');
 const User = require('../../models/user');
 
 describe('User login tests', () => {
+  // beforeEach(done => {
+  //   User.collection.remove();
+  //   done();
+  // });
 
   beforeEach(done => {
-    User.collection.remove();
-    done();
+    api
+      .post('/api/register')
+      .set('Accept', 'application/json')
+      .send({
+        firstName: 'Sandra',
+        lastName: 'Okoli',
+        email: 'test12345@ga.co',
+        avatar: 'http://fillmurray.com/300/300',
+        password: 'password',
+        passwordConfirmation: 'password'
+      })
+      .end(() => {
+        done();
+      });
   });
 
   afterEach(done => {
@@ -15,23 +31,6 @@ describe('User login tests', () => {
   });
 
   describe('POST /api/login', () => {
-    beforeEach(done => {
-      api
-        .post('/api/register')
-        .set('Accept', 'application/json')
-        .send({
-          firstName: 'Sandra',
-          lastName: 'Okoli',
-          email: 'test12345@ga.co',
-          avatar: 'http://fillmurray.com/300/300',
-          password: 'password',
-          passwordConfirmation: 'password'
-        })
-        .end(() => {
-          done();
-        });
-    });
-
     it('should return a 200 response', done => {
       api
         .post('/api/login')
@@ -45,6 +44,39 @@ describe('User login tests', () => {
           expect(res.body).to.be.a('object');
           expect(res.body.message).to.eq('Welcome back Sandra!');
           expect(res.body.token).to.be.a('string');
+          done();
+        });
+    });
+
+    it('should not login a user without an email', done => {
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          password: 'password'
+        })
+        .end((err, res) => {
+          expect(res.status).to.eq(401);
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.eq('Unauthorised.');
+          expect(Object.keys(res.body)).to.not.include('token');
+          done();
+        });
+    });
+
+    it('should not login a user with the wrong password', function(done) {
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'test12345@ga.co',
+          password: 'pass'
+        })
+        .end((err, res) => {
+          expect(res.status).to.eq(401);
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.eq('Unauthorised.');
+          expect(Object.keys(res.body)).to.not.include('token');
           done();
         });
     });
